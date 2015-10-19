edba0fe219bd05f429142928cdf6aa0b2e185c33
kedien01 <kedien01@luther.edu>
2015-10-19 14:22:56 -0500

    Update todo.js

--- todo.js
+++ todo.js
@@ -1,1 +1,15 @@
 
+<label for= "r"> input </label>
+        <input type="text" id="a" value="hey">
+        <button onclick="Func();"> New </button>
+        <script type="text/javascript">
+         Fun = function() {
+         var y=document.querySelector('#man').value;
+         ol = document.createElement ("input");
+         ol.type = "checkbox";
+         lst1 = document.createElement("input"); 
+         document.body.appendChild(ol);
+         
+     lst1contents=document.createTextNode("document.querySelector('#man').value;");
+         lst1.appendChild(lst1contents);
+         ol.appendChild(lst1);
