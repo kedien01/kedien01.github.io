7fb911d9d670983faccc91874765a225d698c269
kedien01 <kedien01@luther.edu>
2015-10-21 20:34:42 -0400

    Update todo.js

--- todo.js
+++ todo.js
@@ -1,22 +1,28 @@
 
  NewText = function(){
-t = document.querySelector("main")
-ul = document.createElement("ul")
-li = document.createElement("li")
-ch = document.createElement("input")
-ch.type = "checkbox"
-li1 = document.createTextNode(document.querySelector('#redi').value)
-li.appendChild(ch)
-li.appendChild(li1)
-li.className = document.querySelector('#priority').value
-ul.appendChild(li)
-t.appendChild(ul)
-}
-boxChecked = function(){
-if (this.checked){
-this.parentNode.classList.add("Done");
-}
-else{
-this.parentNode.classList.romove("Done");
-}
-}
+  t = document.querySelector("main")
+  ul = document.createElement("ul")
+  li = document.createElement("li")
+  ch = document.createElement("input")
+  ch.type = "checkbox"
+  ch.onclick= boxchecked;
+  li1 = document.createTextNode(document.querySelector('#redi').value)
+  li.appendChild(ch)
+  li.appendChild(li1)
+  li.className = document.querySelector('#priority').value
+  ul.appendChild(li)
+  t.appendChild(ul)
+  localSave("tasklist");
+  
+ }
+ boxChecked = function(){
+  if (this.checked){
+   this.parentNode.classList.add("Done");
+   localSave("tasklist");
+   
+  }
+  else{
+   this.parentNode.classList.romove("Done");
+   localSave("tasklist");
+   }
+ }
